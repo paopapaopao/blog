d5287e2d71fb75c406c53522e0a8db578b965077
paopapaopao <paolo.casugay@gmail.com>
2021-07-24 17:12:16 +0800

    Updated: routes for index, new and create & views for index & show

--- config/routes.rb
+++ config/routes.rb
@@ -1,4 +1,8 @@
 Rails.application.routes.draw do
   resources :articles
   # For details on the DSL available within this file, see https://guides.rubyonrails.org/routing.html
+  get '/articles' => 'articles#index'
+  get '/articles/new' => 'articles#new'#, as: 'new_article'
+  post '/articles' => 'articles#create'#, as: 'create_article'
+  root to: 'articles#index'
 end
