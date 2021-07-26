3496396ab53b3e67e0b11648428052191baf0bed
paopapaopao <paolo.casugay@gmail.com>
2021-07-26 17:33:50 +0800

    Added: destroy comments

--- app/models/article.rb
+++ app/models/article.rb
@@ -2,5 +2,5 @@ class Article < ApplicationRecord
   validates :name, presence: true, uniqueness: true
   validates :body, presence: true, length: {minimum: 5}
 
-  has_many :comments
+  has_many :comments, :dependent => :destroy
 end
