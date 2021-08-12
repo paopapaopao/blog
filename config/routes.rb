aa9e1ac08025bab66ca5ef43385eb4fe0db84d10
paopapaopao <paolo.casugay@gmail.com>
2021-08-12 16:31:08 +0800

    Refactor: routes

--- config/routes.rb
+++ config/routes.rb
@@ -1,11 +1,9 @@
 Rails.application.routes.draw do
+  # For details on the DSL available within this file, see https://guides.rubyonrails.org/routing.html
+
+  root to: 'articles#index'
+
   resources :articles do
     resources :comments
   end
-  
-  # For details on the DSL available within this file, see https://guides.rubyonrails.org/routing.html
-  get '/articles' => 'articles#index'
-  get '/articles/new' => 'articles#new'#, as: 'new_article'
-  post '/articles' => 'articles#create'#, as: 'create_article'
-  root to: 'articles#index'
 end
