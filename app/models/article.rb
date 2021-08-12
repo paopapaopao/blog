034b59eaf2e2c0ff57c975525fff3f34ee24b7b4
paopapaopao <paolo.casugay@gmail.com>
2021-08-12 16:23:28 +0800

    Add: tags

--- app/models/article.rb
+++ app/models/article.rb
@@ -1,6 +1,12 @@
 class Article < ApplicationRecord
+  attr_accessor :tags_attributes
+
   validates :name, presence: true, uniqueness: true
   validates :body, presence: true, length: {minimum: 5}
 
   has_many :comments, :dependent => :destroy
+  has_many :tags
+
+  accepts_nested_attributes_for :tags, :allow_destroy => :true,
+    :reject_if => proc { |attrs| attrs.all? { |k, v| v.blank? } }
 end
