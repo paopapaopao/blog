068976c225ef18c7f4e4111b17e4a97eb0112b5a
paopapaopao <paolo.casugay@gmail.com>
2021-08-18 17:35:40 +0800

    Update: tags association

--- app/models/article.rb
+++ app/models/article.rb
@@ -6,7 +6,7 @@ class Article < ApplicationRecord
 
   belongs_to :user
   has_many :comments, :dependent => :destroy
-  has_many :tags
+  has_many :tags, :dependent => :destroy
 
   accepts_nested_attributes_for :tags, :allow_destroy => :true,
     :reject_if => proc { |attrs| attrs.all? { |k, v| v.blank? } }
