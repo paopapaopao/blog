c56b887e277e8c94bf88fe88f1b93d9f7aea30f5
paopapaopao <paolo.casugay@gmail.com>
2021-08-18 18:05:29 +0800

    Update: article model

--- app/models/article.rb
+++ app/models/article.rb
@@ -2,11 +2,11 @@ class Article < ApplicationRecord
   attr_accessor :tags_attributes
 
   validates :name, presence: true, uniqueness: true
-  validates :body, presence: true, length: {minimum: 5}
+  validates :body, presence: true, length: { minimum: 5 }
 
   belongs_to :user
-  has_many :comments, :dependent => :destroy
-  has_many :tags, :dependent => :destroy
+  has_many :comments, dependent: :destroy
+  has_many :tags, dependent: :destroy
 
   accepts_nested_attributes_for :tags, :allow_destroy => :true,
     :reject_if => proc { |attrs| attrs.all? { |k, v| v.blank? } }
