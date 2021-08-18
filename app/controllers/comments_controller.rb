3119482189d4a22b791065095af5266a812e669b
paopapaopao <paolo.casugay@gmail.com>
2021-08-18 18:01:38 +0800

    Update: comments controller

--- app/controllers/comments_controller.rb
+++ app/controllers/comments_controller.rb
@@ -1,6 +1,4 @@
 class CommentsController < ApplicationController
-  http_basic_authenticate_with :name => "user", :password => "password", :only => :destroy
-
   def create
     @article = Article.find(params[:article_id])
     @comment = @article.comments.create(comment_params)
