2b5b96c40f9763c0a27353b01337fc75055c63f9
paopapaopao <paolo.casugay@gmail.com>
2021-08-25 23:18:48 +0800

    Add data to seeds.rb

--- db/seeds.rb
+++ db/seeds.rb
@@ -5,3 +5,28 @@
 #
 #   movies = Movie.create([{ name: 'Star Wars' }, { name: 'Lord of the Rings' }])
 #   Character.create(name: 'Luke', movie: movies.first)
+
+User.create(
+  email: 'admin@example.com',
+  password: :password,
+  confirmed_at: Time.now.utc,
+  admin: true
+)
+
+malakas = User.create(
+  email: 'malakas@example.com',
+  password: :password,
+  confirmed_at: Time.now.utc
+)
+maganda = User.create(
+  email: 'maganda@example.com',
+  password: :password,
+  confirmed_at: Time.now.utc
+)
+
+4.times do
+  FactoryBot.create(:article, user_id: malakas.id)
+  FactoryBot.create(:article, user_id: maganda.id)
+end
+# or
+# FactoryBot.create_list(:article, 4)
