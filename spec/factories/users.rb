99b7d9df4f0ad932d53bbb07bf6190bc64a35593
paopapaopao <paolo.casugay@gmail.com>
2021-08-26 11:47:22 +0800

    Add confirmed_at on build and create for testing purposes

--- spec/factories/users.rb
+++ spec/factories/users.rb
@@ -2,5 +2,10 @@ FactoryBot.define do
   factory :user do
     email { FFaker::Internet.email }
     password { FFaker::Internet.password }
+
+    confirmed_at { Time.now.utc }
+    # or
+    # after(:build) { |u| u.skip_confirmation_notification! }
+    # after(:create) { |u| u.confirm }
   end
 end
