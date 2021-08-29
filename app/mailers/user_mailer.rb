dc2e7005641f5e84691a01706309ebafa7a5510d
paopapaopao <paolo.casugay@gmail.com>
2021-08-29 08:39:51 +0800

    Generate a mailer for user

--- app/mailers/user_mailer.rb
+++ app/mailers/user_mailer.rb
@@ -5,12 +5,25 @@ class UserMailer < ApplicationMailer
   #
   #   en.user_mailer.user_created.subject
   #
-  def welcome_user
+
+  # sent to users after admin has approved their sign up
+  def user_approved
     @user = User.last
 
     mail(
       to: @user.email,
-      subject: 'Welcome to bloggerista!'
+      subject: 'Approved sign up'
+    )
+  end
+
+  # sent to admin for approval after a user has signed up
+  def admin_new_user_approval(id)
+    @id = id
+
+    mail(
+      # email address devise will use to send emails (e.g. 'eadd@example.com')
+      to: '',
+      subject: 'New user awaiting approval'
     )
   end
 end
