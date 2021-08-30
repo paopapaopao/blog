d38bc07efaf41dd6f836f44a30aa32d9bbac69b6
paopapaopao <paolo.casugay@gmail.com>
2021-08-30 19:32:51 +0800

    Update routes

--- config/routes.rb
+++ config/routes.rb
@@ -1,12 +1,23 @@
 Rails.application.routes.draw do
-  mount RailsAdmin::Engine => '/admin', as: 'rails_admin'
   # For details on the DSL available within this file, see https://guides.rubyonrails.org/routing.html
 
   root to: 'pages#home'
 
   devise_for :users
 
-  resources :articles do
-    resources :comments
+  # allow only logged in users to access the articles and comments paths
+  devise_scope :user do
+    authenticated :user do
+      namespace :users do
+        resources :articles do
+          resources :comments
+        end
+      end
+    end
+  end
+
+  # allow only users with attribute admin set to true to access the admin page
+  authenticate :user, lambda { |u| u.admin == true } do
+    mount RailsAdmin::Engine => '/admin', as: 'rails_admin'
   end
 end
