e271180d2afa564f86289e422cc19c14a59fa73d
paopapaopao <paolo.casugay@gmail.com>
2021-08-31 16:00:00 +0800

    Update migration and schema files

--- db/migrate/20210827080908_add_approved_to_user.rb
+++ db/migrate/20210827080908_add_approved_to_user.rb
@@ -1,5 +1,5 @@
 class AddApprovedToUser < ActiveRecord::Migration[6.1]
   def change
-    add_column :users, :approved, :boolean
+    add_column :users, :approved, :boolean, default: false
   end
 end
--- db/schema.rb
+++ db/schema.rb
@@ -54,7 +54,7 @@ ActiveRecord::Schema.define(version: 2021_08_27_080908) do
     t.datetime "created_at", precision: 6, null: false
     t.datetime "updated_at", precision: 6, null: false
     t.boolean "admin", default: false
-    t.boolean "approved"
+    t.boolean "approved", default: false
     t.index ["email"], name: "index_users_on_email", unique: true
     t.index ["reset_password_token"], name: "index_users_on_reset_password_token", unique: true
   end
