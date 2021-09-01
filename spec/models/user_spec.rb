0884338869593b373a0059257f503cb45bd9db19
paopapaopao <paolo.casugay@gmail.com>
2021-09-01 18:21:16 +0800

    Update user model spec

--- spec/models/user_spec.rb
+++ spec/models/user_spec.rb
@@ -7,15 +7,23 @@ RSpec.describe User, type: :model do
     it do
       subject.email = nil
       expect(subject).to_not be_valid
+      expect(subject.errors).to be_present
+      expect(subject.errors.to_hash.keys).to include(:email)
 
       subject.email = ''
       expect(subject).to_not be_valid
+      expect(subject.errors).to be_present
+      expect(subject.errors.to_hash.keys).to include(:email)
 
       subject.email = ' '
       expect(subject).to_not be_valid
+      expect(subject.errors).to be_present
+      expect(subject.errors.to_hash.keys).to include(:email)
 
       subject.email = (create :user).email
       expect(subject).to_not be_valid
+      expect(subject.errors).to be_present
+      expect(subject.errors.to_hash.keys).to include(:email)
     end
   end
 
@@ -23,23 +31,38 @@ RSpec.describe User, type: :model do
     it do
       subject.password = nil
       expect(subject).to_not be_valid
+      expect(subject.errors).to be_present
+      expect(subject.errors.to_hash.keys).to include(:password)
 
       subject.password = ''
       expect(subject).to_not be_valid
+      expect(subject.errors).to be_present
+      expect(subject.errors.to_hash.keys).to include(:password)
 
       subject.password = ' '
       expect(subject).to_not be_valid
+      expect(subject.errors).to be_present
+      expect(subject.errors.to_hash.keys).to include(:password)
 
       subject.password = 'a' * 5
       expect(subject).to_not be_valid
+      expect(subject.errors).to be_present
+      expect(subject.errors.to_hash.keys).to include(:password)
 
       subject.password = 'a' * 129
       expect(subject).to_not be_valid
+      expect(subject.errors).to be_present
+      expect(subject.errors.to_hash.keys).to include(:password)
     end
   end
 
   context 'When all attributes are valid' do
-    it { expect(subject).to be_valid }
+    it do
+      expect(subject).to be_valid
+      expect(subject.errors).to_not be_present
+      expect(subject.errors.to_hash.keys).to_not include(:email)
+      expect(subject.errors.to_hash.keys).to_not include(:password)
+    end
   end
 
   context 'Association with Articles' do
