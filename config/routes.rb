e0e9348f46b1aaeb4e44e1cac4157b17602e31d8
paopapaopao <paolo.casugay@gmail.com>
2021-10-12 17:43:44 +0800

    Set root route

--- config/routes.rb
+++ config/routes.rb
@@ -1,12 +1,12 @@
 Rails.application.routes.draw do
   # For details on the DSL available within this file, see https://guides.rubyonrails.org/routing.html
 
-  root to: 'pages#home'
-
   devise_for :users
 
   # allow only logged in users to access the articles and comments paths
   authenticate :user do
+    root to: 'articles#index'
+
     resources :articles do
       resources :comments
     end
