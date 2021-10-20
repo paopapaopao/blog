cb634eddf85c0f6cad0fb9b1bd8b7cbf48dd6380
paopapaopao <paolo.casugay@gmail.com>
2021-10-20 13:02:59 +0800

    Update comment model spec

--- spec/models/comment_spec.rb
+++ spec/models/comment_spec.rb
@@ -3,11 +3,17 @@ require 'rails_helper'
 RSpec.describe Comment, type: :model do
   subject { build :comment }
 
-  context 'When all attributes are valid' do
+  context 'When all the attributes are valid' do
     it do
       expect(subject).to be_valid
       expect(subject.errors).to_not be_present
+      expect(subject.errors.to_hash.keys).to eq []
+      expect(subject.errors.to_hash.values).to eq []
     end
+  end
+
+  context 'Association with User' do
+    it { expect(described_class.reflect_on_association(:user).macro).to eq :belongs_to }
   end
 
   context 'Association with Article' do
