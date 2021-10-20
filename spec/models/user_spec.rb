5226c2aaf2814a2a6f18c5c5cb7fbcd9011d8749
paopapaopao <paolo.casugay@gmail.com>
2021-10-20 12:48:14 +0800

    Update user model spec

--- spec/models/user_spec.rb
+++ spec/models/user_spec.rb
@@ -3,65 +3,80 @@ require 'rails_helper'
 RSpec.describe User, type: :model do
   subject { build :user }
 
-  context 'When email is invalid' do
+  context 'When the email attribute is invalid' do
     it do
       subject.email = nil
       expect(subject).to_not be_valid
       expect(subject.errors).to be_present
-      expect(subject.errors.to_hash.keys).to include(:email)
+      expect(subject.errors.to_hash.keys).to include :email
+      expect(subject.errors.to_hash.values[0]).to include "can't be blank"
 
       subject.email = ''
       expect(subject).to_not be_valid
       expect(subject.errors).to be_present
-      expect(subject.errors.to_hash.keys).to include(:email)
+      expect(subject.errors.to_hash.keys).to include :email
+      expect(subject.errors.to_hash.values[0]).to include "can't be blank"
 
       subject.email = ' '
       expect(subject).to_not be_valid
       expect(subject.errors).to be_present
-      expect(subject.errors.to_hash.keys).to include(:email)
+      expect(subject.errors.to_hash.keys).to include :email
+      expect(subject.errors.to_hash.values[0]).to include "can't be blank"
+
+      subject.email = 'a'
+      expect(subject).to_not be_valid
+      expect(subject.errors).to be_present
+      expect(subject.errors.to_hash.keys).to include :email
+      expect(subject.errors.to_hash.values[0]).to include "is invalid"
 
       subject.email = (create :user).email
       expect(subject).to_not be_valid
       expect(subject.errors).to be_present
-      expect(subject.errors.to_hash.keys).to include(:email)
+      expect(subject.errors.to_hash.keys).to include :email
+      expect(subject.errors.to_hash.values[0]).to include "has already been taken"
     end
   end
 
-  context 'When password is invalid' do
+  context 'When the password attribute is invalid' do
     it do
       subject.password = nil
       expect(subject).to_not be_valid
       expect(subject.errors).to be_present
-      expect(subject.errors.to_hash.keys).to include(:password)
+      expect(subject.errors.to_hash.keys).to include :password
+      expect(subject.errors.to_hash.values[0]).to include "can't be blank"
 
       subject.password = ''
       expect(subject).to_not be_valid
       expect(subject.errors).to be_present
-      expect(subject.errors.to_hash.keys).to include(:password)
+      expect(subject.errors.to_hash.keys).to include :password
+      expect(subject.errors.to_hash.values[0]).to include "can't be blank"
 
       subject.password = ' '
       expect(subject).to_not be_valid
       expect(subject.errors).to be_present
-      expect(subject.errors.to_hash.keys).to include(:password)
+      expect(subject.errors.to_hash.keys).to include :password
+      expect(subject.errors.to_hash.values[0]).to include "can't be blank"
 
       subject.password = 'a' * 5
       expect(subject).to_not be_valid
       expect(subject.errors).to be_present
-      expect(subject.errors.to_hash.keys).to include(:password)
+      expect(subject.errors.to_hash.keys).to include :password
+      expect(subject.errors.to_hash.values[0]).to include "is too short (minimum is #{User.password_length.min} characters)"
 
       subject.password = 'a' * 129
       expect(subject).to_not be_valid
       expect(subject.errors).to be_present
-      expect(subject.errors.to_hash.keys).to include(:password)
+      expect(subject.errors.to_hash.keys).to include :password
+      expect(subject.errors.to_hash.values[0]).to include "is too long (maximum is #{User.password_length.max} characters)"
     end
   end
 
-  context 'When all attributes are valid' do
+  context 'When all the attributes are valid' do
     it do
       expect(subject).to be_valid
       expect(subject.errors).to_not be_present
-      expect(subject.errors.to_hash.keys).to_not include(:email)
-      expect(subject.errors.to_hash.keys).to_not include(:password)
+      expect(subject.errors.to_hash.keys).to eq []
+      expect(subject.errors.to_hash.values).to eq []
     end
   end
 
