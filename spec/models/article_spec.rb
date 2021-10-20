1be33815694eb4d59c692a3eded8f4c7ee5b82ee
paopapaopao <paolo.casugay@gmail.com>
2021-10-20 12:56:51 +0800

    Update article model spec

--- spec/models/article_spec.rb
+++ spec/models/article_spec.rb
@@ -3,60 +3,68 @@ require 'rails_helper'
 RSpec.describe Article, type: :model do
   subject { build :article }
 
-  context 'When name is invalid' do
+  context 'When the name attribute is invalid' do
     it do
       subject.name = nil
       expect(subject).to_not be_valid
       expect(subject.errors).to be_present
-      expect(subject.errors.to_hash.keys).to include(:name)
+      expect(subject.errors.to_hash.keys).to include :name
+      expect(subject.errors.to_hash.values[0]).to include "can't be blank"
 
       subject.name = ''
       expect(subject).to_not be_valid
       expect(subject.errors).to be_present
-      expect(subject.errors.to_hash.keys).to include(:name)
+      expect(subject.errors.to_hash.keys).to include :name
+      expect(subject.errors.to_hash.values[0]).to include "can't be blank"
 
       subject.name = ' '
       expect(subject).to_not be_valid
       expect(subject.errors).to be_present
-      expect(subject.errors.to_hash.keys).to include(:name)
+      expect(subject.errors.to_hash.keys).to include :name
+      expect(subject.errors.to_hash.values[0]).to include "can't be blank"
 
       subject.name = (create :article).name
       expect(subject).to_not be_valid
       expect(subject.errors).to be_present
-      expect(subject.errors.to_hash.keys).to include(:name)
+      expect(subject.errors.to_hash.keys).to include :name
+      expect(subject.errors.to_hash.values[0]).to include "has already been taken"
     end
   end
 
-  context 'When body is invalid' do
+  context 'When the body attribute is invalid' do
     it do
       subject.body = nil
       expect(subject).to_not be_valid
       expect(subject.errors).to be_present
-      expect(subject.errors.to_hash.keys).to include(:body)
+      expect(subject.errors.to_hash.keys).to include :body
+      expect(subject.errors.to_hash.values[0]).to include "can't be blank"
 
       subject.body = ''
       expect(subject).to_not be_valid
       expect(subject.errors).to be_present
-      expect(subject.errors.to_hash.keys).to include(:body)
+      expect(subject.errors.to_hash.keys).to include :body
+      expect(subject.errors.to_hash.values[0]).to include "can't be blank"
 
       subject.body = ' '
       expect(subject).to_not be_valid
       expect(subject.errors).to be_present
-      expect(subject.errors.to_hash.keys).to include(:body)
+      expect(subject.errors.to_hash.keys).to include :body
+      expect(subject.errors.to_hash.values[0]).to include "can't be blank"
 
       subject.body = 'a' * 4
       expect(subject).to_not be_valid
       expect(subject.errors).to be_present
-      expect(subject.errors.to_hash.keys).to include(:body)
+      expect(subject.errors.to_hash.keys).to include :body
+      expect(subject.errors.to_hash.values[0]).to include "is too short (minimum is 5 characters)"
     end
   end
 
-  context 'When all attributes are valid' do
+  context 'When all the attributes are valid' do
     it do
       expect(subject).to be_valid
       expect(subject.errors).to_not be_present
-      expect(subject.errors.to_hash.keys).to_not include(:name)
-      expect(subject.errors.to_hash.keys).to_not include(:body)
+      expect(subject.errors.to_hash.keys).to eq []
+      expect(subject.errors.to_hash.values).to eq []
     end
   end
 
