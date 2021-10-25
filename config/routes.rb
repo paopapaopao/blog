e3a8f76e00bba6cc5e94587112f062ff6fc95c74
paopapaopao <paolo.casugay@gmail.com>
2021-10-25 21:48:22 +0800

    Update routes

--- config/routes.rb
+++ config/routes.rb
@@ -1,6 +1,9 @@
 Rails.application.routes.draw do
   # For details on the DSL available within this file, see https://guides.rubyonrails.org/routing.html
 
+  devise_for :admin_users, ActiveAdmin::Devise.config
+  ActiveAdmin.routes(self)
+
   devise_for :users
 
   # allow only logged in users to access the articles and comments paths
