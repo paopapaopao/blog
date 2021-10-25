c8a5eb2783340ffbbd606d319487fb891769e28b
paopapaopao <paolo.casugay@gmail.com>
2021-10-25 14:50:11 +0800

    Update seeds

--- db/seeds.rb
+++ db/seeds.rb
@@ -9,27 +9,32 @@
 User.create(
   email: 'admin@example.com',
   password: :password,
-  confirmed_at: Time.now.utc,
+  confirmed_at: DateTime.now,
   admin: true,
   approved: true
 )
 
-malakas = User.create(
-  email: 'malakas@example.com',
+nightking = User.create(
+  email: 'nightking@example.com',
   password: :password,
-  confirmed_at: Time.now.utc,
+  confirmed_at: DateTime.now,
   approved: true
 )
-maganda = User.create(
-  email: 'maganda@example.com',
+drogon = User.create(
+  email: 'drogon@example.com',
   password: :password,
-  confirmed_at: Time.now.utc,
+  confirmed_at: DateTime.now,
   approved: true
 )
 
 4.times do
-  FactoryBot.create(:article, user_id: malakas.id)
-  FactoryBot.create(:article, user_id: maganda.id)
+  FactoryBot.create(:article, user_id: nightking.id)
+  FactoryBot.create(:comment, user_id: nightking.id, article_id: Article.last.id)
+  FactoryBot.create(:comment, user_id: drogon.id, article_id: Article.last.id)
+
+  FactoryBot.create(:article, user_id: drogon.id)
+  FactoryBot.create(:comment, user_id: nightking.id, article_id: Article.last.id)
+  FactoryBot.create(:comment, user_id: drogon.id, article_id: Article.last.id)
 end
 # or
 # FactoryBot.create_list(:article, 4)
