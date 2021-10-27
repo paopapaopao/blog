9f7df2667d56e27f3e182a47f0a781d32e6ac80b
paopapaopao <paolo.casugay@gmail.com>
2021-10-27 13:15:50 +0800

    Update article model spec

--- spec/models/article_spec.rb
+++ spec/models/article_spec.rb
@@ -6,25 +6,25 @@ RSpec.describe Article, type: :model do
   context 'When the name attribute is invalid' do
     it do
       subject.name = nil
-      expect(subject).to_not be_valid
+      expect(subject).not_to be_valid
       expect(subject.errors).to be_present
       expect(subject.errors.to_hash.keys).to include :name
       expect(subject.errors.to_hash.values[0]).to include "can't be blank"
 
       subject.name = ''
-      expect(subject).to_not be_valid
+      expect(subject).not_to be_valid
       expect(subject.errors).to be_present
       expect(subject.errors.to_hash.keys).to include :name
       expect(subject.errors.to_hash.values[0]).to include "can't be blank"
 
       subject.name = ' '
-      expect(subject).to_not be_valid
+      expect(subject).not_to be_valid
       expect(subject.errors).to be_present
       expect(subject.errors.to_hash.keys).to include :name
       expect(subject.errors.to_hash.values[0]).to include "can't be blank"
 
       subject.name = (create :article).name
-      expect(subject).to_not be_valid
+      expect(subject).not_to be_valid
       expect(subject.errors).to be_present
       expect(subject.errors.to_hash.keys).to include :name
       expect(subject.errors.to_hash.values[0]).to include "has already been taken"
@@ -34,25 +34,25 @@ RSpec.describe Article, type: :model do
   context 'When the body attribute is invalid' do
     it do
       subject.body = nil
-      expect(subject).to_not be_valid
+      expect(subject).not_to be_valid
       expect(subject.errors).to be_present
       expect(subject.errors.to_hash.keys).to include :body
       expect(subject.errors.to_hash.values[0]).to include "can't be blank"
 
       subject.body = ''
-      expect(subject).to_not be_valid
+      expect(subject).not_to be_valid
       expect(subject.errors).to be_present
       expect(subject.errors.to_hash.keys).to include :body
       expect(subject.errors.to_hash.values[0]).to include "can't be blank"
 
       subject.body = ' '
-      expect(subject).to_not be_valid
+      expect(subject).not_to be_valid
       expect(subject.errors).to be_present
       expect(subject.errors.to_hash.keys).to include :body
       expect(subject.errors.to_hash.values[0]).to include "can't be blank"
 
       subject.body = 'a' * 4
-      expect(subject).to_not be_valid
+      expect(subject).not_to be_valid
       expect(subject.errors).to be_present
       expect(subject.errors.to_hash.keys).to include :body
       expect(subject.errors.to_hash.values[0]).to include "is too short (minimum is 5 characters)"
@@ -62,7 +62,7 @@ RSpec.describe Article, type: :model do
   context 'When all the attributes are valid' do
     it do
       expect(subject).to be_valid
-      expect(subject.errors).to_not be_present
+      expect(subject.errors).not_to be_present
       expect(subject.errors.to_hash.keys).to eq []
       expect(subject.errors.to_hash.values).to eq []
     end
