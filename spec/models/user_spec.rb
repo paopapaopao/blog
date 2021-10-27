ae46194e9811bc422c6b3d36af7ac087264d33b4
paopapaopao <paolo.casugay@gmail.com>
2021-10-27 13:17:24 +0800

    Update user model spec

--- spec/models/user_spec.rb
+++ spec/models/user_spec.rb
@@ -6,31 +6,31 @@ RSpec.describe User, type: :model do
   context 'When the email attribute is invalid' do
     it do
       subject.email = nil
-      expect(subject).to_not be_valid
+      expect(subject).not_to be_valid
       expect(subject.errors).to be_present
       expect(subject.errors.to_hash.keys).to include :email
       expect(subject.errors.to_hash.values[0]).to include "can't be blank"
 
       subject.email = ''
-      expect(subject).to_not be_valid
+      expect(subject).not_to be_valid
       expect(subject.errors).to be_present
       expect(subject.errors.to_hash.keys).to include :email
       expect(subject.errors.to_hash.values[0]).to include "can't be blank"
 
       subject.email = ' '
-      expect(subject).to_not be_valid
+      expect(subject).not_to be_valid
       expect(subject.errors).to be_present
       expect(subject.errors.to_hash.keys).to include :email
       expect(subject.errors.to_hash.values[0]).to include "can't be blank"
 
       subject.email = 'a'
-      expect(subject).to_not be_valid
+      expect(subject).not_to be_valid
       expect(subject.errors).to be_present
       expect(subject.errors.to_hash.keys).to include :email
       expect(subject.errors.to_hash.values[0]).to include "is invalid"
 
       subject.email = (create :user).email
-      expect(subject).to_not be_valid
+      expect(subject).not_to be_valid
       expect(subject.errors).to be_present
       expect(subject.errors.to_hash.keys).to include :email
       expect(subject.errors.to_hash.values[0]).to include "has already been taken"
@@ -40,31 +40,31 @@ RSpec.describe User, type: :model do
   context 'When the password attribute is invalid' do
     it do
       subject.password = nil
-      expect(subject).to_not be_valid
+      expect(subject).not_to be_valid
       expect(subject.errors).to be_present
       expect(subject.errors.to_hash.keys).to include :password
       expect(subject.errors.to_hash.values[0]).to include "can't be blank"
 
       subject.password = ''
-      expect(subject).to_not be_valid
+      expect(subject).not_to be_valid
       expect(subject.errors).to be_present
       expect(subject.errors.to_hash.keys).to include :password
       expect(subject.errors.to_hash.values[0]).to include "can't be blank"
 
       subject.password = ' '
-      expect(subject).to_not be_valid
+      expect(subject).not_to be_valid
       expect(subject.errors).to be_present
       expect(subject.errors.to_hash.keys).to include :password
       expect(subject.errors.to_hash.values[0]).to include "can't be blank"
 
       subject.password = 'a' * 5
-      expect(subject).to_not be_valid
+      expect(subject).not_to be_valid
       expect(subject.errors).to be_present
       expect(subject.errors.to_hash.keys).to include :password
       expect(subject.errors.to_hash.values[0]).to include "is too short (minimum is #{User.password_length.min} characters)"
 
       subject.password = 'a' * 129
-      expect(subject).to_not be_valid
+      expect(subject).not_to be_valid
       expect(subject.errors).to be_present
       expect(subject.errors.to_hash.keys).to include :password
       expect(subject.errors.to_hash.values[0]).to include "is too long (maximum is #{User.password_length.max} characters)"
@@ -74,7 +74,7 @@ RSpec.describe User, type: :model do
   context 'When all the attributes are valid' do
     it do
       expect(subject).to be_valid
-      expect(subject.errors).to_not be_present
+      expect(subject.errors).not_to be_present
       expect(subject.errors.to_hash.keys).to eq []
       expect(subject.errors.to_hash.values).to eq []
     end
