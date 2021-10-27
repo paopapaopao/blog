02305cf4c5b258a569c3d6ef40051a446d1d835e
paopapaopao <paolo.casugay@gmail.com>
2021-10-27 13:23:04 +0800

    Update admin_users factory

--- spec/factories/admin_users.rb
+++ spec/factories/admin_users.rb
@@ -1,5 +1,6 @@
 FactoryBot.define do
   factory :admin_user do
-    
+    email { Faker::Internet.unique.email }
+    password { Faker::Internet.password(min_length: 6, max_length: 128) }
   end
 end
