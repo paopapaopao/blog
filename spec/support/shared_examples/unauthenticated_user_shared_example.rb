2773123df3b9485c7701fbbb745af806f7a45b5c
paopapaopao <paolo.casugay@gmail.com>
2021-11-05 18:13:16 +0800

    Create shared example spec for unauthenticated user

---
+++ spec/support/shared_examples/unauthenticated_user_shared_example.rb
@@ -0,0 +1,9 @@
+# frozen_string_literal: true
+
+RSpec.shared_examples "unauthenticated user" do
+  it do
+    expect(response).not_to be_successful
+    expect(response).to have_http_status :found
+    expect(response).to redirect_to new_user_session_path
+  end
+end
