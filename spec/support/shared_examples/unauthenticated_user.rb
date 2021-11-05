f152eddb979243e47bffb83c2c8aacf79bb4b0b0
paopapaopao <paolo.casugay@gmail.com>
2021-11-06 02:02:23 +0800

    Rename unauthenticated user shared example

--- spec/support/shared_examples/unauthenticated_user.rb
+++ spec/support/shared_examples/unauthenticated_user.rb
@@ -1,6 +1,6 @@
 # frozen_string_literal: true
 
-RSpec.shared_examples "unauthenticated user" do
+RSpec.shared_examples 'unauthenticated user' do
   it do
     expect(response).not_to be_successful
     expect(response).to have_http_status :found
