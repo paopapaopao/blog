6afbc1c01fe789ec089d55a54322cc98237d1677
paopapaopao <paolo.casugay@gmail.com>
2021-11-13 21:12:40 +0800

    Create article helper

---
+++ app/helpers/article_helper.rb
@@ -0,0 +1,14 @@
+module ArticlesHelper
+  def format_date_time(article)
+    article.created_at.strftime("%A, %d %B %Y at %H:%M")
+  end
+
+  def created_time_ago(article)
+    distance_of_time_in_words(article.created_at, DateTime.now)
+  end
+
+  def truncate_body(article)
+    # article.body.truncate(400)
+    truncate(strip_tags(article.body.to_s), length: 320)
+  end
+end
