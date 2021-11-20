a0290ff74a2129ce459ef5315ad8259b3c1068e1
paopapaopao <paolo.casugay@gmail.com>
2021-11-20 15:05:56 +0800

    Update

--- spec/factories/tags.rb
+++ spec/factories/tags.rb
@@ -1,6 +1,7 @@
 FactoryBot.define do
   factory :tag do
     name { Faker::Lorem.word }
+
     article
   end
 end
--- spec/factories/users.rb
+++ spec/factories/users.rb
@@ -3,9 +3,6 @@ FactoryBot.define do
     email { Faker::Internet.unique.email }
     password { Faker::Internet.password(min_length: 6, max_length: 128) }
     confirmed_at { DateTime.now }
-    # or
-    # after(:build) { |u| u.skip_confirmation_notification! }
-    # after(:create) { |u| u.confirm }
     approved { true }
   end
 end
